ebd908e72d1c266a30c7cb3a44b5633b06c8a3e7
Colin <colingilbert86@gmail.com>
2016-09-20 16:29:56 -0600

    Initial commit

---
+++ export-ndk-vars.sh
@@ -0,0 +1,8 @@
+export ANDROID_HOME=~/android-sdk-linux &&
+export PATH=$ANDROID_HOME/platform-tools:$PATH &&
+export PATH=$ANDROID_HOME/tools:$PATH &&
+export ANDROID_NDK_HOME=~/android-ndk-r12b &&
+export ANDROID_CMAKE_HOME=~/devel/android-cmake &&
+export ANDROID_STANDALONE_TOOLCHAIN=/home/noob/toolchains &&
+
+# cmake -DCMAKE_TOOLCHAIN_FILE=~/devel/android-cmake/android.toolchain.cmake ..
